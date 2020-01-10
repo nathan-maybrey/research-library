b37ec90d327e4f72451b169be7a53915b5a738e6
Nathan Maybrey <nathan.maybrey@ntlworld.com>
2020-01-10 11:16:36 +0000

    Improved sign in page

--- src/controllers/signin/functions.js
+++ src/controllers/signin/functions.js
@@ -5,12 +5,21 @@ const passport = require('passport');
 
 const signin = (req, res) => {
     res.render(path.join(root, 'src/views/pages', 'signin.html'));
-}
+};
+
+const redirectToAuth = (req, res) => {
+    if(req.body['sign-in'] == 'google'){
+        res.redirect('/signin/google');
+    } else {
+        res.redirect('');
+    }
+};
 
 const handleAuth = (req, res) => {
     console.log("Authenticated");
     res.redirect('/projects');
-}
+};
 
 module.exports.signin = signin;
 module.exports.handleAuth = handleAuth;
+module.exports.redirectToAuth = redirectToAuth;
--- src/controllers/signin/routes.js
+++ src/controllers/signin/routes.js
@@ -6,6 +6,8 @@ const router = new express.Router();
 
 router.get('/', functions.signin);
 
+router.post('/', functions.redirectToAuth);
+
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
 router.get('/redirect', passport.authenticate('google'), functions.handleAuth);
